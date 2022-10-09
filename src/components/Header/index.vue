c555082054c6e21a8371ffe9cc6029765864bce6
user <gregorlitvinov@gmail.com>
2022-10-09 18:32:33 +0300

    title

---
+++ src/components/Header/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <header class = "header" id = "header">
+        <div class = "head-top">
+            <div class = "site-name">
+                <span>GEEKPROBIN</span>
+            </div>
+            <div class = "site-nav">
+                <span id = "nav-btn">MENU <i class = "fas fa-bars"></i></span>
+            </div>
+        </div>
+
+        <div class = "head-bottom flex">
+            <h2>NICE AND COMFORTABLE PLACE TO STAY</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est quos veniam impedit numquam itaque voluptatum, dicta asperiores accusamus, eligendi neque ut incidunt, modi harum molestiae atque natus officia minima.</p>
+            <button type = "button" class = "head-btn">GET STARTED</button>
+        </div>
+    </header>
+
+</template>
+
+<script>
+
+	export default {
+		name: "index"
+	}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from './App.vue';
+import './assets/styles/index.scss';
+
+
+// import router from "./router";
+// import index from "./components/Header/index";
 
 createApp(App).mount('#app')
